2deb3caed84d8e5dc6b38960eeb3f6f57b278ae7
jkbanham <54941260+jkbanham@users.noreply.github.com>
2024-09-12 14:37:10 -0400

    Updates from office Pi work

--- sensor_cloud_azure.py
+++ sensor_cloud_azure.py
@@ -3,8 +3,9 @@ import datetime
 import time
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from w1thermsensor import W1ThermSensor, Unit   # Import temp sensor library
-from azure.iot.device.aio import IoTHubDeviceClient
+from azure.iot.device.aio import IoTHubDeviceClient # Import the Azure IoT modules
 from azure.iot.device import Message
+from colorama import Fore, Back, Style # Use this to change text color on screen output message 
 
 # Setup the GPIO board on the Raspberry Pi...
 GPIO.setwarnings(False)    # Ignore warning for now
@@ -12,18 +13,18 @@ GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)   # Set pin 12 (GPIO 18) to be an output pin and set initial value to low (off)
 
 # Define the connection string and device id for connecting to the Azure IoTHub service...
-#CONNECTION_STRING = "HostName=JKB-IOT.azure-devices.net;DeviceId=PYTHON-TESTER;SharedAccessKey=****"
-CONNECTION_STRING = "HostName=JKB-IOT.azure-devices.net;DeviceId=HOME-OFFICE-PI4;SharedAccessKey=****"
-device_id = "Home-RPI4"
+#connection_string = "HostName=JKB-IOT.azure-devices.net;DeviceId=PYTHON-TESTER;SharedAccessKey=****"
+connection_string = "HostName=JKB-IOT.azure-devices.net;DeviceId=WT-OFFICE-PI;SharedAccessKey=Uaql6YwfjT2H2OG0jVUY372DDTogVdCYpNO6wZImh8I="
+device_id = "WT-OFFICE-PI"
 
 #Define max temp setting above which an alert should be generated and sent...
 max_temp = 80
 
 # Define the JSON message to send to IoT Hub.
-MSG_TXT = '{{"timestamp": "{timenow}","deviceId": {device_id},"temperature": {temperature}}}'
+msg_txt = '{{"timestamp": "{timenow}","deviceId": "{device_id}","temperature": {temperature}}}'
 
 
-async def run_telemetry_sample(client):
+async def iot_device_telemetry(client):  # This is an async function so that...
     print("IoT Hub device sending telemetry messages")
 
     await client.connect()
@@ -38,48 +39,51 @@ async def run_telemetry_sample(client):
         # Get the current temp reading...
         temp = round(sensor.get_temperature(Unit.DEGREES_F),4)
         
-        # print the temp data point to the monitor screen...
-        print ("Current temp reading is ", temp)
-
         # Turn the LED off to indicate data point captured and processed...
         GPIO.output(12, GPIO.LOW)
         time.sleep(2) # Pause for 2 seconds
         
         # Build the message with telemetry values.
         timenow = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        msg_txt_formatted = MSG_TXT.format(timenow=timenow, device_id=device_id, temperature=temp)
+        msg_txt_formatted = msg_txt.format(timenow=timenow, device_id=device_id, temperature=temp)
         message = Message(msg_txt_formatted)
 
         # Add a custom application property to the message.
         # An IoT hub can filter on these properties without access to the message body.
         if temp > max_temp:
-            print("Temp alert flag added to telemetry message due to high value")
+            print ("Current temp reading is ", Fore.RED + str(temp))
+            print("Temp alert flag added to telemetry message due to high temp value")
+            print(Style.RESET_ALL)
             message.custom_properties["temperatureAlert"] = "true"
         else:
+            print ("Current temp reading is ", Fore.GREEN + str(temp))
+            print(Style.RESET_ALL)
             message.custom_properties["temperatureAlert"] = "false"
 
         # Send the message.
         print("Sending message: {}".format(message))
         await client.send_message(message)
         print("Message successfully sent")
-        await asyncio.sleep(20)
+        await asyncio.sleep(10)
 
 def main():
     print ("IoT Hub - Device Telemetry Tracking Tool")
     print ("Press Ctrl-C to exit")
 
     # Instantiate the client. 
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    client = IoTHubDeviceClient.create_from_connection_string(connection_string)
 
+    #
     loop = asyncio.get_event_loop()
     try:
-        # Run the sample in the event loop
-        loop.run_until_complete(run_telemetry_sample(client))
+        # Send telemetry messages while in the event loop
+        loop.run_until_complete(iot_device_telemetry(client))
     except KeyboardInterrupt:
         print("IoTHubClient stopped by user")
     finally:
-        # Upon application exit, shut down the client
+        # Upon application exit, shut down the client and turn off the LED
         print("Shutting down IoTHubClient")
+        GPIO.output(12, GPIO.LOW)
         loop.run_until_complete(client.shutdown())
         loop.close()
 
